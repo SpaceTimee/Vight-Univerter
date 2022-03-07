71c4309f0796cd15883b01c565647643fb9d1336
Space Time <Zeus6_6@163.com>
2022-03-07 12:56:53 +0800

    1.0.0 -> 1.0.1 第3次更新

--- Vight Univerter.Android/Properties/AssemblyInfo.cs
+++ Vight Univerter.Android/Properties/AssemblyInfo.cs
@@ -1,8 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using Android.App;
-
-// General Information about an assembly is controlled through the following 
+﻿// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vight Univerter")]
@@ -18,11 +14,11 @@ using Android.App;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
 
 // Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)] 
--- Vight Univerter.UWP/Properties/AssemblyInfo.cs
+++ Vight Univerter.UWP/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vight Univerter")]
@@ -16,9 +16,9 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: ComVisible(false)]
+[assembly: ComVisible(false)] 
